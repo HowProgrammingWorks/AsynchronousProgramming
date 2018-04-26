ef11d8b194311c49aedcab5a03eaae6a620baaf8
tshemsedinov <timur.shemsedinov@gmail.com>
2018-04-27 01:59:04 +0300

    Prepare async/await example identical to Promise

---
+++ JavaScript/g-async-await.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Emulate Asynchronous calls
+
+const wrapAsync = () => new Promise((resolve, reject) =>
+  setTimeout(resolve, Math.floor((Math.random() * 1000)))
+);
+
+// Asynchronous functions
+
+const readConfig = async (name) => {
+  await wrapAsync();
+  console.log('(1) config loaded');
+  return { name };
+}
+
+const doQuery = async (statement) => {
+  await wrapAsync();
+  console.log('(2) SQL query executed: ' + statement);
+  return [{ name: 'Kiev' }, { name: 'Roma' }];
+};
+
+const httpGet = async (url) => {
+  await wrapAsync();
+  console.log('(3) Page retrieved: ' + url);
+  return '<html>Some archaic web here</html>';
+};
+
+const readFile = async (path) => {
+  await wrapAsync();
+  console.log('(4) Readme file loaded: ' + path);
+  return 'file content';
+};
+
+// Usage
+
+(async () => {
+  const config = await readConfig('myConfig');
+  const res = await doQuery('select * from cities');
+  let json, file;
+  try {
+    json = await httpGet('http://kpi.ua');
+  } catch (err) {
+    console.log('Error (1): ' + err.message);
+  }
+  try {
+    file = await readFile('README.md');
+  } catch (err) {
+    console.log('Reject reason (2): ' + err.message);
+  }
+  console.log('Done');
+  console.dir({ config, res, json, file });
+})();
