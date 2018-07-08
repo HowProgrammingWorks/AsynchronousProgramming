2add1cf049a211b1b74bd3a8b0e21d29708233ac
tshemsedinov <timur.shemsedinov@gmail.com>
2018-07-09 00:02:46 +0300

    Use lambdas and error-first contract

--- JavaScript/1-sequential-callbacks.js
+++ JavaScript/1-sequential-callbacks.js
@@ -1,6 +1,35 @@
 'use strict';
 
-// Sequentian calls and sequentian execution
+// Sequential calls and sequentian execution
+// of 4 pseudo-asynchronous functions
+
+const readConfig = (name, callback) => {
+  setTimeout(() => {
+    console.log('(1) config loaded: ' + name);
+    callback(null, { name });
+  }, 1000);
+};
+
+const doQuery = (statement, callback) => {
+  setTimeout(() => {
+    console.log('(2) SQL query executed: ' + statement);
+    callback(null, [{ name: 'Kiev' }, { name: 'Roma' }]);
+  }, 1000);
+};
+
+const httpGet = (url, callback) => {
+  setTimeout(() => {
+    console.log('(3) Page retrieved: ' + url);
+    callback(null, '<html>Some archaic web here</html>');
+  }, 1000);
+};
+
+const readFile = (path, callback) => {
+  setTimeout(() => {
+    console.log('(4) Readme file loaded');
+    callback(null, 'file content');
+  }, 1000);
+};
 
 console.log('start');
 
@@ -10,34 +39,3 @@ httpGet('http://kpi.ua', () => {});
 readFile('README.md', () => {});
 
 console.log('end');
-
-// Pseudo-Asynchronous Functions
-// having just callbacks but working synchronously
-
-function readConfig(name, callback) {
-  setTimeout(() => {
-    console.log('(1) config loaded: ' + name);
-    callback({ name });
-  }, 1000);
-}
-
-function doQuery(statement, callback) {
-  setTimeout(() => {
-    console.log('(2) SQL query executed: ' + statement);
-    callback([ { name: 'Kiev' }, { name: 'Roma' } ]);
-  }, 1000);
-}
-
-function httpGet(url, callback) {
-  setTimeout(() => {
-    console.log('(3) Page retrieved: ' + url);
-    callback('<html>Some archaic web here</html>');
-  }, 1000);
-}
-
-function readFile(path, callback) {
-  setTimeout(() => {
-    console.log('(4) Readme file loaded');
-    callback('file content');
-  }, 1000);
-}
