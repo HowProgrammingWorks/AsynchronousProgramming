9f13a09f0b6902d45c87d901a74eb3bf5c1c9a3c
Maksym Shenderuk <jaood96@gmail.com>
2024-01-12 00:55:41 +0200

    Add simplified callback chaining implementations
    PR-URL: https://github.com/HowProgrammingWorks/AsynchronousProgramming/pull/12
    Co-authored-by: Timur Shemsedinov <timur.shemsedinov@gmail.com>

---
+++ JavaScript/5a-chain.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// Use list and chaining syntax to build sequence
+
+const chain = () => {
+  const callbacks = [];
+
+  const nextCallback = () => callbacks.shift();
+
+  const forward = (err, data) => {
+    if (err) return console.log({ err });
+    if (data) console.log('Next callback will get:', { data });
+
+    const next = nextCallback();
+    if (!next) return;
+
+    const [callback, name] = next;
+    console.log('Calling:', name);
+    // Pass data from previous callback
+    callback(data, forward);
+  };
+
+  forward.do = (func, name) => {
+    callbacks.push([func, name]);
+    return forward;
+  };
+  
+  return forward;
+};
+
+// Emulate asynchronous calls
+
+const wrapAsync = (fn) => (...args) => setTimeout(
+  () => fn(...args), Math.floor(Math.random() * 1000)
+);
+
+// Asynchronous functions
+
+const readConfig = wrapAsync((name, callback) => {
+  console.log('(1) config loaded');
+  callback(null, { name });
+});
+
+const selectFromDb = wrapAsync((query, callback) => {
+  console.log('(2) SQL query executed');
+  callback(null, [{ name: 'Kiev' }, { name: 'Roma' } ]);
+});
+
+const getHttpPage = wrapAsync((url, callback) => {
+  console.log('(3) Page retrieved');
+  callback(null, '<html>Some archaic web here</html>');
+});
+
+const readFile = wrapAsync((path, callback) => {
+  console.log('(4) Readme file loaded');
+  callback(null, 'file content');
+});
+
+// Usage
+
+const startChain = chain()
+  .do(readConfig, 'myConfig')
+  .do(selectFromDb, 'select * from cities')
+  .do(getHttpPage, 'http://kpi.ua')
+  .do(readFile, 'README.md');
+
+startChain();
